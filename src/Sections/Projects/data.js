266e3cf774a6f812d96f5dd1a471795d9a99aa1c
Armen <khalilyan1001@mail.ru>
2023-08-01 22:44:21 +0400

    updated

--- src/Sections/Projects/data.js
+++ src/Sections/Projects/data.js
@@ -23,6 +23,7 @@ export const projects = [
         image: require('./images/OPENAI.jpg'),
         about: `This project provides an opportunity to ask your questions to ChatGPT and receive a voice response.
         (To ask a question, hold up on the microphone icon and speak your question. Then, wait for a response.)`
+        `WARNING: API key not working`
     },
     {
         name: 'Todo List',
