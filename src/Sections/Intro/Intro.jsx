a0e137292a03b60df60d91509d53e4cf73c00b6f
Armen <khalilyan1001@mail.ru>
2023-10-22 14:04:12 +0400

    edit cv

--- src/Sections/Intro/Intro.jsx
+++ src/Sections/Intro/Intro.jsx
@@ -7,10 +7,10 @@ export default function Intro() {
   return (
     <section id='0' className='IntroContainer' >
         <div className='IntroTxtContainer'>
-          <svg id='title' width="100%" height="100" >
-            <text id='text' x="50%" y="70%" dominantBaseline="middle" textAnchor="middle"  className='FDev' >Frontend Developer.</text>
+          <svg id='title' width="100%" height="250" >
+            <text id='text' x="50%" y="70%" dominantBaseline="middle" textAnchor="middle"  className='FDev' >Frontend Developer</text>
           </svg>
-          <svg id='mobileTitle' width="100%" height="120" >
+          <svg id='mobileTitle' width="100%" height="170" >
             <text id='text' x="50%" y="30%" dominantBaseline="middle" textAnchor="middle"  className='FDev' >Frontend </text>
             <text id='text' x="50%" y="80%" dominantBaseline="middle" textAnchor="middle"  className='FDev' >Developer</text>
           </svg>
@@ -24,7 +24,7 @@ export default function Intro() {
             initial={{y: 50,opacity: 0}}
             animate={{y: 0,opacity: 1}}
             transition={{duration: 2,delay: 3.5,ease: 'easeInOut'}}
-            >A person who Creates beautiful and functional websites.</motion.p>
+            ></motion.p>
             </div>
         </div>
         <div className='ScrollContain' >
